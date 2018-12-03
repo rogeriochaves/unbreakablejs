3a92dbc8896063e7142847c7382d39b177fb0b41
Rogerio Chaves <rogeriocfj@gmail.com>
2018-12-03 22:49:08 +0100

    Introducing ReturnValue to allow multiple return values

--- src/Interpreter.elm
+++ src/Interpreter.elm
@@ -1,4 +1,4 @@
-module Interpreter exposing (run, runSymbol)
+module Interpreter exposing (ReturnValue(..), run, runSymbol)
 
 import Dict exposing (Dict)
 import Parser exposing (DeadEnd, Problem(..))
@@ -8,7 +8,6 @@ import Types exposing (..)
 type alias State =
     { variables : Dict String Float
     , functions : Dict String FunctionSchema
-    , error : Maybe DeadEnd
     }
 
 
@@ -16,15 +15,20 @@ newState : State
 newState =
     { variables = Dict.empty
     , functions = Dict.empty
-    , error = Nothing
     }
 
 
+type ReturnValue
+    = NumVal Float
+    | VoidVal
+    | ErrorVal DeadEnd
+
+
 type alias LineResult =
-    ( State, Float )
+    ( State, ReturnValue )
 
 
-run : Types.Program -> Result Error (List Float)
+run : Types.Program -> Result Error (List ReturnValue)
 run expressions =
     let
         iterate : Expression -> Result Error (List LineResult) -> Result Error (List LineResult)
@@ -36,17 +40,17 @@ run expressions =
             let
                 lastLineResult =
                     List.head acc
-                        |> Maybe.withDefault ( newState, 0 )
+                        |> Maybe.withDefault ( newState, VoidVal )
 
                 lineResult =
                     runExpression (Tuple.first lastLineResult) expr
             in
-            case (Tuple.first lineResult).error of
-                Nothing ->
-                    Ok (lineResult :: acc)
-
-                Just error ->
+            case Tuple.second lineResult of
+                ErrorVal error ->
                     Err [ error ]
+
+                _ ->
+                    Ok (lineResult :: acc)
     in
     expressions
         |> List.foldl iterate (Ok [])
@@ -57,60 +61,61 @@ runExpression : State -> Expression -> LineResult
 runExpression state expr =
     case expr of
         Integer val ->
-            ( state, toFloat val )
+            ( state, NumVal (toFloat val) )
 
         Floating val ->
-            ( state, val )
+            ( state, NumVal val )
 
         Identifier name ->
             -- TODO: break if variable is not available
-            ( state, Dict.get name state.variables |> Maybe.withDefault 0 )
+            ( state, Dict.get name state.variables |> Maybe.withDefault 0 |> NumVal )
 
         Addition e1 e2 ->
-            ( state, getExpressionValue state e1 + getExpressionValue state e2 )
+            ( state, applyExpressions state e1 (+) e2 )
 
         Subtraction e1 e2 ->
-            ( state, getExpressionValue state e1 - getExpressionValue state e2 )
+            ( state, applyExpressions state e1 (-) e2 )
 
         Multiplication e1 e2 ->
-            ( state, getExpressionValue state e1 * getExpressionValue state e2 )
+            ( state, applyExpressions state e1 (*) e2 )
 
         Division e1 e2 ->
-            ( state, getExpressionValue state e1 / getExpressionValue state e2 )
+            ( state, applyExpressions state e1 (/) e2 )
 
         Exponentiation e1 e2 ->
-            ( state, getExpressionValue state e1 ^ getExpressionValue state e2 )
+            ( state, applyExpressions state e1 (^) e2 )
 
         SymbolicFunction symbol ->
             runSymbol state symbol
 
         Equation variableName e ->
-            let
-                lineResult =
-                    getExpressionValue state e
-            in
-            ( setVariable variableName lineResult state, lineResult )
+            case getExpressionValue state e of
+                NumVal num ->
+                    ( setVariable variableName num state, VoidVal )
+
+                VoidVal ->
+                    ( state, throwError ("Cannot set variable" ++ variableName ++ "to void") )
+
+                ErrorVal error ->
+                    ( state, ErrorVal error )
 
         FunctionDeclaration name schema ->
-            ( setFunction name schema state, 0 )
+            ( setFunction name schema state, VoidVal )
 
         FunctionCall name param ->
             let
                 callFunction (FunctionSchema paramName body) =
-                    let
-                        param_ =
-                            getExpressionValue state param
-
-                        state_ =
-                            setVariable paramName param_ state
-                    in
-                    getExpressionValue state_ body
+                    getExpressionValue state param
+                        |> returnAndThen
+                            (\param_ ->
+                                getExpressionValue (setVariable paramName param_ state) body
+                            )
             in
             -- TODO: break if function is not available
             ( state
             , Dict.get name state.functions
                 |> Maybe.map callFunction
-                |> Maybe.withDefault 0
+                |> Maybe.withDefault (NumVal 0)
             )
 
 
@@ -120,44 +125,45 @@ runSymbol state symbol =
         SingleArity sym expr1 ->
             case sym of
                 Sqrt ->
-                    ( state, sqrt (getExpressionValue state expr1) )
+                    ( state, applyExpression state sqrt expr1 )
 
         DoubleArity sym expr1 expr2 ->
             case sym of
                 Frac ->
-                    ( state, getExpressionValue state expr1 / getExpressionValue state expr2 )
+                    ( state, applyExpressions state expr1 (/) expr2 )
 
         Iterator sym identifier expr1 expr2 expr3 ->
             case sym of
                 Sum_ ->
-                    let
-                        lowerLimit =
-                            getExpressionValue state expr1
-
-                        upperLimit =
-                            getExpressionValue state expr2
-
-                        range =
-                            List.range (round lowerLimit) (round upperLimit)
-
-                        iterate curr total =
+                    ( state
+                    , returnAndThen2
+                        (\lowerLimit upperLimit ->
                             let
-                                state_ =
-                                    setVariable identifier (toFloat curr) state
+                                range =
+                                    List.range (round lowerLimit) (round upperLimit)
+
+                                iterate curr total =
+                                    let
+                                        state_ =
+                                            setVariable identifier (toFloat curr) state
+                                    in
+                                    map2NumReturn (\result total_ -> total_ + result) (getExpressionValue state_ expr3) total
                             in
-                            total + getExpressionValue state_ expr3
-                    in
-                    if lowerLimit /= toFloat (round lowerLimit) then
-                        throwError ("Error on sum_: cannot use " ++ String.fromFloat lowerLimit ++ " as a lower limit, it has to be an integer") state
+                            if lowerLimit /= toFloat (round lowerLimit) then
+                                throwError ("Error on sum_: cannot use " ++ String.fromFloat lowerLimit ++ " as a lower limit, it has to be an integer")
 
-                    else if upperLimit /= toFloat (round upperLimit) || upperLimit < lowerLimit then
-                        throwError ("Error on sum_: cannot use " ++ String.fromFloat upperLimit ++ " as an upper limit, it has to be an integer higher than lower limit") state
+                            else if upperLimit /= toFloat (round upperLimit) || upperLimit < lowerLimit then
+                                throwError ("Error on sum_: cannot use " ++ String.fromFloat upperLimit ++ " as an upper limit, it has to be an integer higher than lower limit")
 
-                    else
-                        ( state, List.foldl iterate 0 range )
+                            else
+                                List.foldl iterate (NumVal 0) range
+                        )
+                        (getExpressionValue state expr1)
+                        (getExpressionValue state expr2)
+                    )
 
 
-getExpressionValue : State -> Expression -> Float
+getExpressionValue : State -> Expression -> ReturnValue
 getExpressionValue state =
     runExpression state >> Tuple.second
 
@@ -172,6 +178,80 @@ setFunction name functionSchema state =
     { state | functions = Dict.insert name functionSchema state.functions }
 
 
-throwError : String -> State -> LineResult
-throwError error state =
-    ( { state | error = Just { row = 0, col = 0, problem = Problem error } }, 0 )
+throwError : String -> ReturnValue
+throwError error =
+    ErrorVal { row = 0, col = 0, problem = Problem error }
+
+
+mapNumReturn : (Float -> Float) -> ReturnValue -> ReturnValue
+mapNumReturn fn returnVal =
+    case returnVal of
+        NumVal float ->
+            NumVal <| fn float
+
+        VoidVal ->
+            throwError "Cannot apply function to void"
+
+        ErrorVal _ ->
+            returnVal
+
+
+map2NumReturn : (Float -> Float -> Float) -> ReturnValue -> ReturnValue -> ReturnValue
+map2NumReturn fn returnVal returnVal2 =
+    case ( returnVal, returnVal2 ) of
+        ( NumVal float1, NumVal float2 ) ->
+            NumVal <| fn float1 float2
+
+        ( ErrorVal _, _ ) ->
+            returnVal
+
+        ( _, ErrorVal _ ) ->
+            returnVal2
+
+        ( VoidVal, _ ) ->
+            throwError "Cannot apply function to void"
+
+        ( _, VoidVal ) ->
+            throwError "Cannot apply function to void"
+
+
+applyExpressions : State -> Expression -> (Float -> Float -> Float) -> Expression -> ReturnValue
+applyExpressions state e1 fn e2 =
+    map2NumReturn fn (getExpressionValue state e1) (getExpressionValue state e2)
+
+
+applyExpression : State -> (Float -> Float) -> Expression -> ReturnValue
+applyExpression state fn e1 =
+    mapNumReturn fn (getExpressionValue state e1)
+
+
+returnAndThen : (Float -> ReturnValue) -> ReturnValue -> ReturnValue
+returnAndThen fn returnVal =
+    case returnVal of
+        NumVal float ->
+            fn float
+
+        VoidVal ->
+            throwError "Cannot apply function to void"
+
+        ErrorVal _ ->
+            returnVal
+
+
+returnAndThen2 : (Float -> Float -> ReturnValue) -> ReturnValue -> ReturnValue -> ReturnValue
+returnAndThen2 fn returnVal returnVal2 =
+    case ( returnVal, returnVal2 ) of
+        ( NumVal float1, NumVal float2 ) ->
+            fn float1 float2
+
+        ( ErrorVal _, _ ) ->
+            returnVal
+
+        ( _, ErrorVal _ ) ->
+            returnVal2
+
+        ( VoidVal, _ ) ->
+            throwError "Cannot apply function to void"
+
+        ( _, VoidVal ) ->
+            throwError "Cannot apply function to void"
--- tests/InterpreterTest.elm
+++ tests/InterpreterTest.elm
@@ -15,69 +15,69 @@ suite =
                 \_ ->
                     MathParser.parse "1 + 1"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 2 ])
+                        |> Expect.equal (Ok [ NumVal 2 ])
             , test "sum float numbers" <|
                 \_ ->
                     MathParser.parse "1.5 + 1.3"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 2.8 ])
+                        |> Expect.equal (Ok [ NumVal 2.8 ])
             , test "execute nested expressions" <|
                 \_ ->
                     MathParser.parse "1 - (3 - 2)"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 0 ])
+                        |> Expect.equal (Ok [ NumVal 0 ])
             , test "respects math priority" <|
                 \_ ->
                     MathParser.parse "2 + 3 * 2"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 8 ])
+                        |> Expect.equal (Ok [ NumVal 8 ])
             , test "respects math priority #2" <|
                 \_ ->
                     MathParser.parse "2 * 3 + 2"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 8 ])
+                        |> Expect.equal (Ok [ NumVal 8 ])
             , test "symbol function aplication with other expression" <|
                 \_ ->
                     MathParser.parse "\\sqrt{9} + 2"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 5 ])
+                        |> Expect.equal (Ok [ NumVal 5 ])
             , test "symbol function aplication on a expression" <|
                 \_ ->
                     MathParser.parse "\\sqrt{7 + 2}"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 3 ])
+                        |> Expect.equal (Ok [ NumVal 3 ])
             , test "exponentiation" <|
                 \_ ->
                     MathParser.parse "2 ^ 5"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 32 ])
+                        |> Expect.equal (Ok [ NumVal 32 ])
             , test "respects math priority #3" <|
                 \_ ->
                     MathParser.parse "2 * 3 ^ 5"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 486 ])
+                        |> Expect.equal (Ok [ NumVal 486 ])
             ]
         , describe "symbols"
             [ test "sqrt" <|
                 \_ ->
                     MathParser.parse "\\sqrt{9}"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 3 ])
+                        |> Expect.equal (Ok [ NumVal 3 ])
             , test "frac" <|
                 \_ ->
                     MathParser.parse "\\frac{3}{2}"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 1.5 ])
+                        |> Expect.equal (Ok [ NumVal 1.5 ])
             , test "summation" <|
                 \_ ->
                     MathParser.parse "\\sum_{x=1}^{3} 5"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 15 ])
+                        |> Expect.equal (Ok [ NumVal 15 ])
             , test "summation using the variable" <|
                 \_ ->
                     MathParser.parse "\\sum_{x=1}^{3} x + 1"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 9 ])
+                        |> Expect.equal (Ok [ NumVal 9 ])
             , test "summation with a float upper limit should break" <|
                 \_ ->
                     MathParser.parse "\\sum_{x=1}^{3.9} 5"
@@ -119,18 +119,18 @@ suite =
             \_ ->
                 MathParser.parse "1 + 1\n2 + 2"
                     |> Result.andThen Interpreter.run
-                    |> Expect.equal (Ok [ 2, 4 ])
+                    |> Expect.equal (Ok [ NumVal 2, NumVal 4 ])
         , describe "equations" <|
-            [ test "parses a simple equation and return the result of the variabl" <|
+            [ test "parses a simple equation and return void" <|
                 \_ ->
                     MathParser.parse "x = 2 + 2"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 4 ])
+                        |> Expect.equal (Ok [ VoidVal ])
             , test "saves the value to the variable" <|
                 \_ ->
                     MathParser.parse "x = 2 + 2\nx + 1"
                         |> Result.andThen Interpreter.run
-                        |> Expect.equal (Ok [ 4, 5 ])
+                        |> Expect.equal (Ok [ VoidVal, NumVal 5 ])
             ]
         , describe "functions"
             [ test "declares a simple function" <|
@@ -138,6 +138,6 @@ suite =
                     MathParser.parse "f(x) = x + 1\nf(5)"
                         |> Result.andThen Interpreter.run
                         -- TODO: function declarations shouldn return anything
-                        |> Expect.equal (Ok [ 0, 6 ])
+                        |> Expect.equal (Ok [ VoidVal, NumVal 6 ])
             ]
         ]
