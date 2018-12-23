97cc1a3925e3194d47dd566aeee4eb2426541c57
Rogerio Chaves <rogeriocfj@gmail.com>
2018-12-24 00:41:35 +0100

    Add notebook-like functionality to playground

--- src/Playground.elm
+++ src/Playground.elm
@@ -1,5 +1,6 @@
 module Playground exposing (main)
 
+import AutoExpand as AutoExpand
 import Browser
 import Dict
 import Html exposing (..)
@@ -7,7 +8,9 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Keyed
 import Interpreter
+import List.Extra
 import MathParser
+import Playground.Style as Style
 
 
 main : Program () Model Msg
@@ -21,18 +24,37 @@ main =
 
 
 type alias Model =
+    { cells : List Cell
+    }
+
+
+type alias Cell =
     { input : String
+    , autoexpand : AutoExpand.State
     , result : String
     }
 
 
 type Msg
-    = UpdateInput String
+    = UpdateInput Int { textValue : String, state : AutoExpand.State }
+    | AddCell
 
 
 init : () -> ( Model, Cmd Msg )
 init flags =
-    ( { input = "", result = "" }, Cmd.none )
+    ( { cells =
+            [ newCell 0
+            ]
+      }
+    , Cmd.none
+    )
+
+
+newCell index =
+    { input = ""
+    , autoexpand = AutoExpand.initState (autoExpandConfig index)
+    , result = ""
+    }
 
 
 emptyLatexState =
@@ -46,26 +68,43 @@ emptyLatexState =
 
 view : Model -> Html Msg
 view model =
-    div []
+    div (Style.body ++ [ style "padding" "20px", style "margin" "-8px" ])
         [ h1 [] [ text "Explain Math" ]
-        , h2 [] [ text "latex code input" ]
-        , textarea [ onInput UpdateInput ] [ text model.input ]
-        , h2 [] [ text "latex output" ]
-        , renderLatex model
-        , h2 [] [ text "result" ]
-        , text model.result
+        , toolbar
+        , div (Style.notebook ++ [ style "padding" "20px" ])
+            (List.indexedMap cell model.cells)
         ]
 
 
-renderLatex model =
+toolbar : Html Msg
+toolbar =
+    div [ style "padding-bottom" "20px" ]
+        [ button [ onClick AddCell ] [ text "Add" ]
+        , button [] [ text "Run" ]
+        ]
+
+
+cell : Int -> Cell -> Html Msg
+cell index item =
+    div []
+        [ AutoExpand.view (autoExpandConfig index) item.autoexpand item.input
+        , h2 [] [ text "latex output" ]
+        , renderLatex item
+        , h2 [] [ text "result" ]
+        , text item.result
+        ]
+
+
+renderLatex : Cell -> Html Msg
+renderLatex item =
     let
         convertedText =
-            "$$\n" ++ String.replace "\n" "\\\\" model.input ++ "\n$$"
+            "$$\n" ++ String.replace "\n" "\\\\" item.input ++ "\n$$"
     in
     div []
         [ Html.Keyed.node "div"
             []
-            [ ( model.input, div [ class "raw-math" ] [ text <| convertedText ] )
+            [ ( item.input, div [ class "raw-math" ] [ text <| convertedText ] )
             ]
         ]
 
@@ -73,13 +112,36 @@ renderLatex model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        UpdateInput input ->
-            ( { model
-                | input = input
-                , result =
-                    MathParser.parse input
-                        |> Result.andThen Interpreter.run
-                        |> Debug.toString
-              }
+        UpdateInput index { state, textValue } ->
+            let
+                updateCell cell_ =
+                    { cell_
+                        | autoexpand = state
+                        , input = textValue
+                        , result =
+                            MathParser.parse textValue
+                                |> Result.andThen Interpreter.run
+                                |> Debug.toString
+                    }
+            in
+            ( { model | cells = List.Extra.updateIfIndex ((==) index) updateCell model.cells }
             , Cmd.none
             )
+
+        AddCell ->
+            ( { model | cells = model.cells ++ [ newCell (List.length model.cells) ] }
+            , Cmd.none
+            )
+
+
+autoExpandConfig : Int -> AutoExpand.Config Msg
+autoExpandConfig index =
+    AutoExpand.config
+        { onInput = UpdateInput index
+        , padding = 12
+        , lineHeight = 21
+        , minRows = 1
+        , maxRows = 4
+        }
+        |> AutoExpand.withAttribute (Html.Attributes.style "resize" "none")
+        |> AutoExpand.withAttribute (Html.Attributes.style "width" "100%")
---
+++ src/Playground/Style.elm
@@ -0,0 +1,14 @@
+module Playground.Style exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+body =
+    [ style "font-family" "sans-serif"
+    , style "background" "#EEE"
+     ]
+
+notebook =
+  [ style "background" "#FFF"
+  ]
